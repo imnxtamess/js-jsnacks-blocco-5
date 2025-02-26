d64c35c0bb15517e37d2e5a4daefbdbd174fe2a1
imnxtamess <davide.morisco.work@gmail.com>
2025-02-26 13:57:55 +0100

    add snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+findStudent = students.find((student) => (student.name === "Marco Lanci"))
+
+console.log(findStudent.class);
