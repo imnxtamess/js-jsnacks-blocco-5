85c7888d7f172457f9b105a46660025ef3bcba1f
imnxtamess <davide.morisco.work@gmail.com>
2025-02-26 14:13:13 +0100

    add bonus snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -6,3 +6,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 const evenNums = nums.filter((number) => number % 2 === 0)
 
 console.log(evenNums);
+
+// bonus
+
+const numeriPari = []
+
+for (let i = 0; i < nums.length; i++) {
+  const thisNum = nums[i];
+  if (thisNum % 2 === 0) {
+    numeriPari.push(thisNum)
+  }
+}
+
+console.log(numeriPari);
