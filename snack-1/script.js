fd6d1da3826d72fed64a1285421f93cb1b8311e1
imnxtamess <davide.morisco.work@gmail.com>
2025-02-26 13:42:51 +0100

    add snack 1 and 2

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((name) => {
+  console.log(name);
+
+})
